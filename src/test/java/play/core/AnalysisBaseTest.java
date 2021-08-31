8644bd7d698f61b0cfffd2e3b1fd9d9ef8c399fa
aspaans <aspaans@CE45305.corp.capgemini.com>
2021-08-31 09:15:24 +0200

    Test infrastructure

---
+++ src/test/java/play/core/AnalysisBaseTest.java
@@ -0,0 +1,14 @@
+package play.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AnalysisBaseTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
