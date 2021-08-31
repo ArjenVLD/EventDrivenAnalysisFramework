58b188547a16b675f44c36f92775c04caf2ae149
aspaans <aspaans@CE45305.corp.capgemini.com>
2021-08-31 22:06:04 +0200

    fix import

--- src/test/java/play/core/AnalysisBaseTest.java
+++ src/test/java/play/core/AnalysisBaseTest.java
@@ -2,7 +2,7 @@ package play.core;
 
 
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
